de4548760f791cf28f9658a038e607009c988bcd
Sakshi Agarwal <devsakshiagarwal@gmail.com>
2020-05-25 16:31:26 +0400

    signin page ui

---
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widget/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    @required String assetName,
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  })  : assert(text != null),
+        assert(assetName != null),
+        super(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image.asset(assetName),
+                Text(
+                  text,
+                  style: TextStyle(color: textColor, fontSize: 15.0),
+                ),
+                Opacity(
+                    opacity: 0,
+                    child: Image.asset(
+                      assetName,
+                    )),
+              ],
+            ),
+            color: color,
+            height: 40.0,
+            onPressed: () {});
+}
